ad6d30324214fa9b39afd55062fe3957a2ec8a46
Ali Abdel Aal <aliabdelaal369@gmail.com>
2019-05-18 03:11:14 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+import logging
+import json
+from flask import Flask, request, jsonify
+import requests
+import telegram
+from telegram.ext import Updater
+from telebot.credentials import bot_token, bot_user_name,URL
+
+
+global bot
+global TOKEN
+TOKEN = bot_token
+bot = telegram.Bot(token=TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+
+    response = "معلش !"
+    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+
+    return 'ok'
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+@app.route('/')
+def index():
+    return '.'
+
+
+if __name__ == '__main__':
+    app.run(threaded=True)
---
+++ telebot/credentials.py
@@ -0,0 +1,3 @@
+bot_token = "811553267:AAGuXanSU83uEnlzxKD6ozJOVioCPDIZ8tI"
+bot_user_name = "thegithubot"
+URL = "https://heruko-hello-flask.herokuapp.com/"
